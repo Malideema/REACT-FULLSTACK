82d43cd3b7ecad01fa2bf61687c9b8fbd57b0aac
Melody <mrobateau@livedigi.com>
2024-09-25 11:41:26 -0600

    Episode 8

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ app.use("/posts", postRouter);
 const commentsRouter = require("./routes/Comments");
 app.use("/comments", commentsRouter);
 
+const usersRouter = require("./routes/Users");
+app.use("/auth", commentsRouter);
+
+
 //Connect to database
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
