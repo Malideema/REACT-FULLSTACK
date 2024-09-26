3e776eb06528f9d8eefb2edb7c2aebbfdcf5e4fe
Melody <mrobateau@livedigi.com>
2024-09-26 08:17:27 -0600

    Episode 8 v2

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,26 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { Users }= require('../models');
+const { Users } = require("../models");
 const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => {
-    const { username, password } = req.body;
-    bcrypt.hash(password, 10).then((hash) => {
-        Users.create({ username: username, password: hash,});
-        return res.json("User created successfully");
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    Users.create({
+      username: username,
+      password: hash,
     });
+    res.json("SUCCESS");
+  });
 });
 
 router.post("/login", async (req, res) => {
     const { username, password } = req.body;
-
+  
     const user = await Users.findOne({ where: { username: username } });
+  
+    if (!user) {
+        res.json({ error: "User Doesn't Exist" });
+      }else {
+           bcrypt.compare(password, user.password).then((match) => {
+              if (!match) {
+                  res.json({ error: "Wrong Username And Password Combination" });
+              } else {
+                  res.json("YOU LOGGED IN!!!");
+              }
+          }); 
+      }
+  });
 
-    if (!user) return res.status(404).json("User Doesn't Exist");
-    bcrypt.compare(password, user.password).then((match) => {
-        if (!match) return res.status(401).json("Wrong Username and Password Combination");
-        else {return res.json("Login successful")};
-    });
-});
-
-module.exports = router; 
+module.exports = router;
